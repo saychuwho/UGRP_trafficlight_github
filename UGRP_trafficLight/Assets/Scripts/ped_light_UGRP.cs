5e5c183f1abec7ca8d1b5154a608969f2301a7fd
saychuwho_HASS <saychuwho@dgist.ac.kr>
2023-07-17 17:30:17 +0900

    final try of commiting

---
+++ UGRP_trafficLight/Assets/Scripts/ped_light_UGRP.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ped_light_UGRP : traffic_light_UGRP
+{
+    // ped light의 경우, 자신이 고려해야 하는 차량 신호등의 정보를 저장할 필요가 있다.
+    // 보행자 신호등을 기준으로 정면 신호등
+    public int pedFrontTrafficlight = 0;
+    // 보행자 신호등을 기준으로 왼쪽 차량 신호등
+    public int pedSideTrafficlight01 = 0;
+    // 보행자 신호등을 기준으로 오른쪽 차량 신호등
+    public int pedSideTrafficlight02 = 0;
+
+    public int pedIndicator01 = 0;
+
+    // 보행자 신호등을 기준으로 오른쪽 우회전 신호등
+    public int pedControlRightlight = 0;
+}
